6e0bff8dbea72297a5be4e84600915a8833a546a
Xue <setsuodu@users.noreply.github.com>
2026-05-17 02:22:52 +0800

    update AnimeModel

--- Anime.Avalonia/Anime.Avalonia/Models/AnimeModel.cs
+++ Anime.Avalonia/Anime.Avalonia/Models/AnimeModel.cs
@@ -1,9 +1,32 @@
-﻿namespace Anime.Avalonia.Models;
+﻿using System.Collections.Generic;
+
+namespace Anime.Avalonia.Models;
 
 public class AnimeModel
 {
-    public string? Title { get; set; }
-    public string? Description { get; set; }
-    // 根据你后端 JSON 再加其他字段，比如 Poster、Year、Rating 等
-    // JsonSerializer 已经开了 PropertyNameCaseInsensitive，所以大小写不敏感
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string JapaneseTitle { get; set; }
+
+    public string EnglishTitle { get; set; }
+
+    public string Episodes { get; set; }
+
+    public string CoverUrl { get; set; }
+
+    public string SourceFingerprint { get; set; }
+
+    public string PlayUrls { get; set; }
+
+    public string BackupUrls { get; set; }
+
+    public int Year { get; set; } // 存储年份（如 2026）
+
+    public string Area { get; set; }
+
+    public string Category { get; set; }
+
+    public System.DateTime UpdateTime { get; set; }
 }
