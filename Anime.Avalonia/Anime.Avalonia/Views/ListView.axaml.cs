b5f7891bde541f3bb4c94e8e9ae459037df8e85a
Xue <setsuodu@users.noreply.github.com>
2026-05-17 02:41:35 +0800

    改竖屏，一行两个

--- Anime.Avalonia/Anime.Avalonia/Views/ListView.axaml.cs
+++ Anime.Avalonia/Anime.Avalonia/Views/ListView.axaml.cs
@@ -1,8 +1,27 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Anime.Avalonia.Models;
+using Anime.Avalonia.ViewModels;
 
 namespace Anime.Avalonia.Views;
 
 public partial class ListView : UserControl
 {
-    public ListView() => InitializeComponent();
+    public ListView()
+    {
+        InitializeComponent();
+    }
+
+    // 处理条目点击事件
+    private void OnItemPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is Control control && control.DataContext is AnimeModel selectedAnime)
+        {
+            if (DataContext is ListViewModel vm)
+            {
+                // 触发 ViewModel 中的属性变更，进而执行跳转逻辑
+                vm.SelectedItem = selectedAnime;
+            }
+        }
+    }
 }
